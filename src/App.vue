7e8523ef94c7f211efb5d93dee8117238bdc5d55
João Copetti <32890601+JoaoHCopetti@users.noreply.github.com>
2026-01-08 18:38:38 -0300

    feat: improve booted logic

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { onBeforeMount, onUnmounted, ref } from 'vue'
-import AppTransition from './components/AppTransition.vue'
+import { onBeforeMount, onBeforeUnmount, ref } from 'vue'
 import ToastContainer from './components/toast/ToastContainer.vue'
 import { useLocalStorage } from './composables/use-local-storage'
 import MainSidebar from './layout/main-sidebar/MainSidebar.vue'
@@ -12,7 +11,7 @@ import { useShortcutsStore } from './stores/shortcuts-store'
 import { useToastStore } from './stores/toast-store'
 import { LocalStorageEnum } from './utils/enums'
 
-const isBooted = ref<boolean>(false)
+const isReady = ref<boolean>(false)
 
 const appStore = useAppStore()
 const shortcutsStore = useShortcutsStore()
@@ -27,21 +26,22 @@ onBeforeMount(async () => {
   if (lastConnection) {
     try {
       await appStore.init(lastConnection.provider, lastConnection.host)
+      isReady.value = true
     } catch (error) {
       console.error(error)
       toastStore.error("Couldn't restore your last provider connection", { timeout: 5000 })
     }
   }
-
-  isBooted.value = true
 })
 
-const initApp = async ({ provider, host }: ProviderConnectionEvents['confirmed']) => {
+const onConnectionConfirmed = async ({ provider, host }: ProviderConnectionEvents['confirmed']) => {
   await appStore.init(provider, host)
+
   storage.setItem(LocalStorageEnum.LastConnection, { provider, host })
+  isReady.value = true
 }
 
-onUnmounted(() => {
+onBeforeUnmount(() => {
   shortcutsStore.destroy()
 })
 </script>
@@ -52,22 +52,13 @@ onUnmounted(() => {
 
     <MainSidebar class="-ml-[100%] w-full sm:ml-0 sm:w-[300px] md:w-[400px]" />
 
-    <AppTransition
-      from-class="opacity-0"
-      to-class="opacity-100"
-      active-class="transition-all duration-1000 ease-out"
-    >
-      <div
-        v-if="isBooted"
-        class="relative w-full"
-      >
-        <RouterView v-if="appStore.provider" />
+    <div class="relative w-full">
+      <RouterView v-if="appStore.provider && isReady" />
 
-        <ProviderConnection
-          v-else
-          @confirmed="initApp"
-        />
-      </div>
-    </AppTransition>
+      <ProviderConnection
+        v-else-if="!appStore.provider"
+        @confirmed="onConnectionConfirmed"
+      />
+    </div>
   </main>
 </template>
