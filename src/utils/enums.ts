062c732ce07f10432f51b781f9956756eb96cd92
João Copetti <32890601+JoaoHCopetti@users.noreply.github.com>
2026-01-08 15:29:21 -0300

    chore: remove unused enum

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -2,7 +2,6 @@ export enum LocalStorageEnum {
   SelectedPromptId = 'SELECTED_PROMPT_ID',
   SelectedModelId = 'SELECTED_MODEL_ID',
   Think = 'THINK',
-  OllamaApiKey = 'OLLAMA_API_KEY',
   LastConnection = 'LAST_CONNECTION',
 }
 
